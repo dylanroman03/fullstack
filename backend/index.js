d14e19b1f8562ae6fa77af1ea0e32cca6c283719
dylanroman03 <romand.buitrago@gmail.com>
2020-05-14 11:35:39 -0400

    express initialize

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
-//Initialize App
+//Initialize
 const app = express()
 
 //Settings express
 app.set('port', 3000);
 
+//Middlewares
+app.use(morgan('dev'))
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, 'public/uploads'),
+  filename(req, file, cb) {
+    cb(null, new Data().getTime() + path.extname(file.originalname))
+  }
+})
+
+app.use(multer({storage}).single('image'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 //Listen App
 app.listen(app.get('port'), () => {
-  console.log('Service active')
+  console.log('Service active on ', app.get('port'))
 })
